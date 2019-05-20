4159f2498a65f74ef4f3e6a3c153dc9ffdb921dc
straycatse <developer@straycat.nu>
2019-05-20 12:26:32 +0200

    Subtitle added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Hello World!</h1>
+        <p>Nu testar jag</p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
